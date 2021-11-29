2aaa8e2973620bbf84f5bbcb95495aafb46d21d0
gabriell.luna <gabriell.luna@delfos.com>
2021-11-29 13:12:19 -0300

    initial commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/gabriell/Documents/USP/SCC0904/pothole-report/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
