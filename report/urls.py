8d67874c06526622a4974edb6566f4be8a09d802
Gabriell Tavares <gabrielltavsb@gmail.com>
2021-12-07 00:05:24 -0300

    update templates

--- report/urls.py
+++ report/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.report_list, name='report_list'),
+    path('report/<int:pk>/', views.report_detail, name='report_detail'),
 ]
--- report/views.py
+++ report/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Report
 
 def report_list(request): 
     reports = Report.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
     return render(request, 'report/report_list.html', {'reports': reports})
+
+def report_detail(request, pk):
+    report = get_object_or_404(pk=pk) 
+    return render(request, 'report/report_detail.html', {'report': report})
