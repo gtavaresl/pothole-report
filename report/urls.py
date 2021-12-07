4463378a7c24c8fd703afb45c6aaa8cbd33909f5
Gabriell Tavares <gabrielltavsb@gmail.com>
2021-12-07 00:40:59 -0300

    add forms

--- report/urls.py
+++ report/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.report_list, name='report_list'),
     path('report/<int:pk>/', views.report_detail, name='report_detail'),
+    path('report/new/', views.report_new, name='report_new'),
 ]
--- report/views.py
+++ report/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Report
+from .forms import ReportForm
 
 def report_list(request): 
-    reports = Report.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    reports = Report.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') 
     return render(request, 'report/report_list.html', {'reports': reports})
 
 def report_detail(request, pk):
-    report = get_object_or_404(pk=pk) 
+    report = get_object_or_404(Report, pk=pk) 
     return render(request, 'report/report_detail.html', {'report': report})
+
+def report_new(request):
+    if request.method == "POST":
+        form = ReportForm(request.POST)
+        if form.is_valid():
+            report = form.save(commit=False)
+            report.author = request.user
+            report.publish()
+            return redirect('report_detail', pk=report.pk)
+    else:
+        form = ReportForm()
+    return render(request, 'report/report_edit.html', {'form': form})
